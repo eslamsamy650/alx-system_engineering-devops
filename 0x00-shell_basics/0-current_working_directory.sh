558f211dabdcf48bb7174f08622f0e270f89db92
EslamSamy <eslamsamy650@gmail.com>
2023-08-02 05:16:16 -0700

    Shell Basics

---
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get the second absolute path name of the current working directory.
+current_directory=$(pwd -P)
+
+# Print the second absolute path name of the current working directory.
+echo "$current_directory"
+
